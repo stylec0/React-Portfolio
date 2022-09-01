828e4b4aaeca724af28c815e539c56c6981c8cc9
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-08-31 22:04:46 -0700

    updated header CSS for main image to match media query

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,14 +6,12 @@ import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
   return (
+    <header>
      <div className="container header__container">
        <h5>Hello I'm</h5>
        <h1>Charles Co</h1>
        <h5 className='text-light'>Fullstack Developer</h5>
        <CTA></CTA>
-       <br/>
-       <br/>
-       <br/>
        <HeaderSocial></HeaderSocial>
 
        <div className='me'>
@@ -22,6 +20,7 @@ const Header = () => {
 
          <a href='#contact' className='scroll_down'>Scroll Down</a>
        </div> 
+       </header>
   )
 }
 
