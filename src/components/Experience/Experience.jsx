0eb2f95e3bbdc2a33e90c55753338c0745805289
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-09-01 19:27:09 -0700

    Experienced JSX and CSS, not complete on css

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,127 @@
 import React from 'react'
 import './Experience.css'
+import { Icon } from 'semantic-ui-react'
+
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience_container">
+        <div className='experience_frontend'>
+          <h3>Frontend Development</h3>
+          <div className="experience_content">
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>HTML</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>CSS</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Javascript</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Bootstrap</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>React</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Angular</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* End of front end */}
+
+        <div className='experience_backend'>
+          <h3>Backend Development</h3>
+          <div className="experience_content">
+          <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Node.js</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>MongoDB</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>PostGresSQL</h4>
+              <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Express</h4>
+              <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Google Auth</h4>
+              <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>AWS</h4>
+              <small className='text-light'>Basic</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Django</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+            <article className='experience_details'>
+              <Icon size='large'name='checkmark box'></Icon>
+              <div>
+              <h4>Python</h4>
+              <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+      </div>
+      </div>
+        </div>
+    </section>
   )
 }
 
