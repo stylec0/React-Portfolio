fd308fed2380a9b81e4b90351e4861001f8f4542
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-09-21 20:25:11 -0700

    Footer and Nav bar update complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Footer.css'
+import { Icon } from 'semantic-ui-react'
 
 const Footer = () => {
   return (
@@ -8,12 +9,22 @@ const Footer = () => {
 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Experience</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
+
+      <div className="footer_socials">
+        <a href='https://facebook.com'><Icon className="footer_icon" size='large' name='facebook'></Icon></a>
+        <a href='https://instagram.com'><Icon className="footer_icon" size='large' name='instagram'></Icon></a>
+        <a href='https://twitter.com'><Icon className="footer_icon" size='large' name='twitter'></Icon></a>
+      </div>
+
+    <div className="footer_copyright">
+     {/*Add Copyright here &copy; ColiveStream CLS Casters. All rights reserved*/}
+    </div>
     </footer>
   )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -10,19 +10,22 @@ const Nav = () => {
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? "active" : ""}>
       <Icon size ="big" name="home"></Icon>
       </a>
-      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === 'about' ? 'active' : ""}>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ""}>
       <Icon size ="big" name="user circle"></Icon>
       </a>
-      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === 'experience' ? 'active' : ""}>
-      <Icon size ="big" name="book"></Icon>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ""}>
+      <Icon size ="big" name="graduation cap"></Icon>
       </a>
-      <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === 'projects' ? 'active' : ""}>
-      <Icon size ="big" name="talk"></Icon>
+      <a href='#projects' onClick={() => setActiveNav('#projects')} className={activeNav === '#projects' ? 'active' : ""}>
+      <Icon size ="big" name="folder open"></Icon>
       </a>
       {/*<a href='#services' onClick={() => setActiveNav('#services')} className={activeNav === 'services' ? 'active' : ""}>
       <Icon size ="big" name="talk"></Icon>
       </a> ---------- Disabled until real services can be added*/}
-      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === 'contact' ? 'active' : ""}>
+      <a href='#recommendations' onClick={() => setActiveNav('#recommendations')} className={activeNav === '#recommendations' ? 'active' : ""}>
+      <Icon size ="big" name="talk"></Icon>
+      </a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ""}>
       <Icon size ="big" name="phone"></Icon>
       </a>
     </nav>
