58ef4378a278f2d2fed8f8d9370f5ac6427e4a30
Charles Co <stylec0-mac@Charless-MacBook-Air.local>
2022-09-22 20:04:53 -0700

    added transition to recommendations, +1 recommendation

--- src/components/Recommendations/Recommendations.jsx
+++ src/components/Recommendations/Recommendations.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Recommendations.css'
 import PIC1 from '../../assets/ArthurD.jpg'
 import PIC2 from '../../assets/Jason-Nguyen.jpg'
+import PIC3 from '../../assets/Elsbeth.png'
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -24,13 +25,17 @@ const data = [
     name: 'Jason Nguyen',
     review: 'If you are looking for a committed and passionate professional who excels in the IT field, Charles is no doubt a strong candidate for IT and business recruiters. Not only is he capable of resolving IT-related problems with critical thinking skills, Charles is capable at breaking down problems or projects to a step-by-step results-driven method for his team and himself. Throughout the Learning and Development phase at Year Up, he consistently surprised me with his quick ability to adopt feedback and apply them to furthering his own professional experience and to others. Charles is a great team dynamic when accomplishing a task or project goal. He is incredibly diverse in his background in graphic, programming, and windows-based platforms. Without a doubt, I am looking forward to his bright career and potential to excel in all that he does.'
   },
+  {
+    pic: PIC3,
+    name: 'Elsbeth Cockcroft',
+    review: 'Charles is a self-motivator that is willing to work for the good of the whole. His personality and experiences have made him an ideal candidate for working with others. He is not a “me first” kind of person; instead Charles wants to see everyone achieve success. I was repeatedly impressed with Charles’s decision making skills and his willingness to work for what must be done. Charles is a man who truly understands the meaning of responsibility and commitment; if he is set to a task not only will he complete it, but it will be done to the best of his ability in a manner that goes above and beyond the stated expectations. '
+  },
 ]
 
 
 const Recommendations = () => {
   return (
     <section id='recommendations'>
-      <h5>Recommendations from Clients</h5>
       <h2>Recommendations</h2>
 
       <Swiper className="container recommendations_container"
